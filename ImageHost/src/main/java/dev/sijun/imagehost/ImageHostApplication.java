673f047b47797c139bee6377be36b3428e09d554
Yang Sijun <yang@sijun.dev>
2025-04-24 09:37:06 +0900

    Init Spring Boot Project

---
+++ ImageHost/src/main/java/dev/sijun/imagehost/ImageHostApplication.java
@@ -0,0 +1,13 @@
+package dev.sijun.imagehost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageHostApplication.class, args);
+    }
+
+}
